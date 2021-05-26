83b99e95bd89a2d298b7ab9ff0581d254a46a19a
rotator-cell <78510453+rotator-cell@users.noreply.github.com>
2021-05-27 00:40:00 +0200

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,1 +1,1 @@
-﻿document.write( '<img src=\"https://images-cdn.9gag.com/photo/anYxVZb_700b.jpg\" style=\"width: 150px; height: auto; position: absolute; top:0px; left:0px;\"/>' );
+﻿document.write( '<img src=\"https://images-cdn.9gag.com/photo/anYxVZb_700b.jpg\" style=\"z-index: 999999;width: 150px; height: auto; position: absolute; top:0px; left:0px;\"/>' );
